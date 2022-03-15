ab0bd1e256a42b140872a3af1fc8a830d052d532
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2022-03-15 16:19:50 -0600

    update to the code
    I wasn't sure what to do for the PCoffset since that's typically a label, so I made it a number to indicate how far away the label is from the instruction

--- src/js/scriptM.js
+++ src/js/scriptM.js
@@ -139,17 +139,111 @@ function createTask(event) {
         )
         updateStorage(tasks);
     }
+    //check for each type of assembly instruction
     let trans = '';
-    if (stringy.substr(0, 4) == '0001') {
+    if (stringy.substr(0, 4) == '0001') { //ADD Instruction
         trans = 'ADD '
-        trans += findR(stringy.substr(4, 3)), ' ';
-        trans += findR(stringy.substr(7, 3)), ' ';
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += findR(stringy.substr(7, 3)), ', ';
         if (stringy[10] == 0) {
-            trans += findR(stringy.substr(13, 3)), ' ';
+            trans += findR(stringy.substr(13, 3));
         }
         else {
             trans += '#' , parseInt(stringy.substr(11, 15), 2);
         }
+    }
+    if (stringy.substr(0, 4) == '0101') { //AND Instruction
+        trans = 'AND '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += findR(stringy.substr(7, 3)), ', ';
+        if (stringy[10] == 0) {
+            trans += findR(stringy.substr(13, 3));
+        }
+        else {
+            trans += '#' , parseInt(stringy.substr(11, 15), 2);
+        }
+    }
+    if (stringy.substr(0, 4) == '0000') { //Branch Instruction - how to do PCoffset
+        trans = 'BR '
+        if (stringy[4] == 1) {
+            trans += 'n'           
+        }
+        if (stringy[5] == 1) {
+            trans += 'z'       
+        }
+        if (stringy[6] == 1) {
+            trans += 'p'           
+        }
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1100') { //JMP Instruction
+        trans = 'JMP '
+        trans += findR(stringy.substr(7, 3));
+    }
+    if (stringy.substr(0, 4) == '0100') { //JSR & JSRR Instruction       
+        if (stringy[4] == 1) {
+            trans = 'JSR '
+            trans += 'PCoffset of ' , parseInt(stringy.substr(5, 15), 2);
+        }
+        else {
+            trans = 'JSRR '
+            trans += findR(stringy.substr(7, 3)), ' ';
+        }
+    }
+    if (stringy.substr(0, 4) == '0010') { //LD Instruction
+        trans = 'LD '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1010') { //LDI Instruction
+        trans = 'LDI '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '0110') { //LDR Instruction
+        trans = 'LDR '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += findR(stringy.substr(7, 3)), ', ';
+        trans += 'offset of ' , parseInt(stringy.substr(10, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1110') { //LEA Instruction
+        trans = 'LEA '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1001') { //NOT Instruction
+        trans = 'NOT '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += findR(stringy.substr(7, 3));
+    }
+    if (stringy.substr(0, 4) == '1100') { //RET Instruction
+        trans = 'RET'
+    }
+    if (stringy.substr(0, 4) == '1000') { //RTI Instruction
+        trans = 'RTI'
+    }
+    if (stringy.substr(0, 4) == '0011') { //ST Instruction
+        trans = 'ST '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1011') { //STI Instruction
+        trans = 'STI '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += 'PCoffset of ' , parseInt(stringy.substr(7, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '0111') { //STR Instruction
+        trans = 'STR '
+        trans += findR(stringy.substr(4, 3)), ', ';
+        trans += findR(stringy.substr(7, 3)), ', ';
+        trans += 'offset of ' , parseInt(stringy.substr(10, 15), 2);
+    }
+    if (stringy.substr(0, 4) == '1111') { //TRAP Instruction
+        trans = 'TRAP'
+        trans += 'parseInt(stringy.substr(8, 15), 2).toString(16).toUpperCase();
+    }
+    if (stringy.substr(0, 4) == '1101') { //reserved Instruction
+        trans = 'reserved'
     }
     //document.write('<p>I am HERE</p>');
 }
